ef740e04085307d6b1602235f3bbe823764c33ec
Mike Zaccardo <mike.zaccardo@cloudsoftcorp.com>
2015-08-26 17:15:01 +0100

    Add show and plan sensors

--- src/main/java/io/cloudsoft/terraform/TerraformConfigurationImpl.java
+++ src/main/java/io/cloudsoft/terraform/TerraformConfigurationImpl.java
@@ -1,8 +1,18 @@
 package io.cloudsoft.terraform;
 
+import org.apache.brooklyn.core.entity.lifecycle.Lifecycle;
+import org.apache.brooklyn.core.entity.lifecycle.ServiceStateLogic;
+import org.apache.brooklyn.core.location.Locations;
 import org.apache.brooklyn.entity.software.base.SoftwareProcessImpl;
 import org.apache.brooklyn.feed.ssh.SshFeed;
+import org.apache.brooklyn.feed.ssh.SshPollConfig;
+import org.apache.brooklyn.feed.ssh.SshPollValue;
+import org.apache.brooklyn.location.ssh.SshMachineLocation;
+import org.apache.brooklyn.util.guava.Maybe;
 import org.apache.brooklyn.util.text.Strings;
+import org.apache.brooklyn.util.time.Duration;
+
+import com.google.common.base.Function;
 
 public class TerraformConfigurationImpl extends SoftwareProcessImpl implements TerraformConfiguration {
 
@@ -19,27 +29,53 @@ public class TerraformConfigurationImpl extends SoftwareProcessImpl implements T
         super.connectSensors();
         connectServiceUpIsRunning();
 
-//        Maybe<SshMachineLocation> machine = Locations.findUniqueSshMachineLocation(getLocations());
-//        if (machine.isPresent()) {
-//            sshFeed = SshFeed.builder()
-//                    .entity(this)
-//                    .period(Duration.PRACTICALLY_FOREVER)
-//                    .machine(machine.get())
-//                    .poll(new SshPollConfig<Boolean>(CONFIGURATION_IS_VALID)
-//                            .command(getDriver().makeTerraformCommand("plan"))
-//                            .onSuccess(new Function<SshPollValue, Boolean>() {
+        Maybe<SshMachineLocation> machine = Locations.findUniqueSshMachineLocation(getLocations());
+        if (machine.isPresent()) {
+            sshFeed = SshFeed.builder()
+                    .entity(this)
+                    .period(Duration.TEN_SECONDS)
+                    .machine(machine.get())
+                    .poll(new SshPollConfig<String>(SHOW)
+                            .command(getDriver().makeTerraformCommand("show"))
+                            .onSuccess(new Function<SshPollValue, String>() {
+                                @Override
+                                public String apply(SshPollValue input) {
+                                    return input.getStdout();
+                                }})
+                            .onFailure(new Function<SshPollValue, String>() {
+                                @Override
+                                public String apply(SshPollValue input) {
+                                    ServiceStateLogic.setExpectedState(TerraformConfigurationImpl.this, Lifecycle.ON_FIRE);
+                                    return input.getStderr();
+                                }}))
+//                    .poll(new SshPollConfig<String>(STATE)
+//                            .command(getDriver().makeTerraformCommand("state"))
+//                            .onSuccess(new Function<SshPollValue, String>() {
 //                                @Override
-//                                public Boolean apply(SshPollValue input) {
-//                                    return true;
+//                                public String apply(SshPollValue input) {
+//                                    return input.getStdout();
 //                                }})
-//                            .onFailure(new Function<SshPollValue, Boolean>() {
+//                            .onFailure(new Function<SshPollValue, String>() {
 //                                @Override
-//                                public Boolean apply(SshPollValue input) {
+//                                public String apply(SshPollValue input) {
 //                                    ServiceStateLogic.setExpectedState(TerraformConfigurationImpl.this, Lifecycle.ON_FIRE);
-//                                    return false;
+//                                    return input.getStderr();
 //                                }}))
-//                    .build();
-//        }
+                    .poll(new SshPollConfig<String>(PLAN)
+                            .command(getDriver().makeTerraformCommand("plan"))
+                            .onSuccess(new Function<SshPollValue, String>() {
+                                @Override
+                                public String apply(SshPollValue input) {
+                                    return input.getStdout();
+                                }})
+                            .onFailure(new Function<SshPollValue, String>() {
+                                @Override
+                                public String apply(SshPollValue input) {
+                                    ServiceStateLogic.setExpectedState(TerraformConfigurationImpl.this, Lifecycle.ON_FIRE);
+                                    return input.getStderr();
+                                }}))
+                    .build();
+        }
     }
 
     @Override
