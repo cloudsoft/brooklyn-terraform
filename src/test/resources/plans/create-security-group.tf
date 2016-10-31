6dcf5ce2d26a5041e7cd6c570c6233f5b995c74d
Sam Corbett <sam.corbett@cloudsoftcorp.com>
2016-10-31 21:49:48 +0000

    TerraformConfiguration entity fixes and simplifications
    * Update pom project name and description
    * Fix use of configurationChangeInProgress
    * Add integration test that creates a security group
    * Fix sensor check when turning output into sensors
    * Apply shell env to SSH feed
    * Simplify driver
    * Simplify check between config URL and contents

---
+++ src/test/resources/plans/create-security-group.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_all" {
+  description = "test-security-group allowing all access"
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "name" {
+    value = "${aws_security_group.allow_all.name}"
+}
+
+output "id" {
+    value = "${aws_security_group.allow_all.id}"
+}
