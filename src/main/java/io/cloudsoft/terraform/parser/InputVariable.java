fffa1fdf22b539f409734efb0a08ac9434fc2aea
zan-mateusz <mateusz.zan@cloudsoftcorp.com>
2021-10-07 14:22:19 +0100

    parsing input variables into model

--- src/main/java/io/cloudsoft/terraform/parser/InputVariable.java
+++ src/main/java/io/cloudsoft/terraform/parser/InputVariable.java
@@ -4,12 +4,51 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class InputVariable {
 
-    String name;
-    String defaultValue;
-    String type;
-    String description;
-    JsonNode validation;
-    Boolean sensitive;
+    private String name = "";
+    private String defaultValue = "";
+    private String type = "";
+    private String description = "";
+    private JsonNode validation;
+    private Boolean sensitive = false;
+
+    public InputVariable () {
+
+    }
 
 
+    // getters
+
+    public String getName() {
+        return name;
+    }
+
+    public Boolean isSensitive() {
+        return sensitive;
+    }
+
+    //setters
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setDefaultValue (String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public void setType (String type) {
+        this.type = type;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+
+    public void setValidation(JsonNode validationNode) {
+        this.validation = validationNode;
+    }
+
+    public void setSensitive(Boolean sensitive) {
+        this.sensitive = sensitive;
+    }
 }
--- src/main/java/io/cloudsoft/terraform/parser/TerraformModel.java
+++ src/main/java/io/cloudsoft/terraform/parser/TerraformModel.java
@@ -5,28 +5,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.testng.util.Strings;
 
+import java.io.InputStream;
 import java.util.*;
 
 public class TerraformModel {
 
-    /*
-    private Set<TerraformModel> childModules = new HashSet<TerraformModel>();
-
-    private Map<String,Object> defaultProvider = new HashMap<String, Object>();
-    private Set<Map<String,Object>> providers = new HashSet<Map<String,Object>>();
-
-    private Set<Resource> resources = new HashSet<Resource>();
-
-    private Set<Map<String,Object>> dataSources = new HashSet<Map<String,Object>>();
-    private Set<Map<String,Object>> localValues = new HashSet<Map<String,Object>>();
-
-    private Set<Map<String,Object>> inputVariables = new HashSet<Map<String,Object>>();
-
-    private Set<Map<String,Object>> output = new HashSet<Map<String,Object>>();
-
-     */
-
     private ObjectMapper objectMapper = new ObjectMapper();
+
     //resources
     Map<String, Resource> resources = new HashMap<String, Resource>();
 
@@ -62,15 +47,14 @@ public class TerraformModel {
 
     private void makeModel(){
         try {
-            // use the configuration and output data to create,
-
             //build up inputs from configuration - parse HCL
             if (configurationData != null) {
+
                 // initially, take resources from configuration (willl be overriden by state/show data)
                 JsonNode resourcesNode = configurationData.at("/resource");
-                Iterator<Map.Entry<String, JsonNode>> fields = resourcesNode.fields();
-                while (fields.hasNext()) {
-                    Map.Entry<String, JsonNode> resourceTypeGroup = fields.next();
+                Iterator<Map.Entry<String, JsonNode>> resourceFields = resourcesNode.fields();
+                while (resourceFields.hasNext()) {
+                    Map.Entry<String, JsonNode> resourceTypeGroup = resourceFields.next();
                     String resourceType = resourceTypeGroup.getKey();
                     JsonNode resourcesOfType = resourceTypeGroup.getValue();
                     Iterator<Map.Entry<String, JsonNode>> resourcesOfTypeIterator = resourcesOfType.fields();
@@ -95,21 +79,56 @@ public class TerraformModel {
 
                 }
 
-                if (resourcesNode.isArray()) {
-                    for (JsonNode resourceNode: resourcesNode){
-                        String resourceType = resourceNode.asText();
-                    }
-                }
 
-                JsonNode a = resourcesNode;
                 // input vars
+                JsonNode inputVariablesNode = configurationData.at("/variable");
+                Iterator<Map.Entry<String, JsonNode>> inputVariablesFields = inputVariablesNode.fields();
+                while (inputVariablesFields.hasNext()) {
+                    Map.Entry<String, JsonNode> inputVariableNode = inputVariablesFields.next();
+
+                    InputVariable variable = new InputVariable();
+                    String variableName = inputVariableNode.getKey();
+                    variable.setName(variableName);
+
+                    Iterator<Map.Entry<String, JsonNode>> variableDataNode = inputVariableNode.getValue().fields();
+                    while (variableDataNode.hasNext()){
+                        Map.Entry<String, JsonNode> variableAttribute = variableDataNode.next();
+                        String variableAttributeName = variableAttribute.getKey();
+                        JsonNode variableAttributeValue = variableAttribute.getValue();
+                        // handle acceptable values:
+                        if (variableAttributeName.equals("default")) {
+                            variable.setDefaultValue(variableAttributeValue.asText());
+                            continue;
+                        }
+                        if (variableAttributeName.equals("description")) {
+                            variable.setDescription(variableAttributeValue.asText());
+                            continue;
+                        }
+                        if (variableAttributeName.equals("type")) {
+                            variable.setType(variableAttributeValue.asText());
+                            continue;
+                        }
+                        if (variableAttributeName.equals("validation")) {
+                            variable.setValidation(variableAttributeValue);
+                            continue;
+                        }
+                        if (variableAttributeName.equals("sensitive")) {
+                            variable.setSensitive(variableAttributeValue.asBoolean());
+                            continue;
+                        }
+                    }
+                    inputVariables.put(variable.getName(), variable);
+
+
+                }
             }
 
 
             if (outputData != null) {
                 //build up resources from output data
                 if (resources.size() > 0) {
-                    // if we already have some resources parsed from the configuration bit, we want to use output as it
+                    // if we already have some resources parsed from the configuration bit,
+                    // we just clear them as output/show is a better representation of actual real world
                     resources.clear();
                 }
                 JsonNode resourcesNode = outputData.at("/values/root_module/resources");
