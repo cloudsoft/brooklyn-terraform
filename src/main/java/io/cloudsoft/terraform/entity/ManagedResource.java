f3dfa0e1ed2f05206282c1fed50f3c8e3eda8689
iuliana <iuliana@cloudsoft.io>
2021-12-08 13:12:03 +0100

    Added different lables, states and entities for startable and non-startable managed resources

--- src/main/java/io/cloudsoft/terraform/entity/ManagedResource.java
+++ src/main/java/io/cloudsoft/terraform/entity/ManagedResource.java
@@ -2,18 +2,12 @@ package io.cloudsoft.terraform.entity;
 
 import org.apache.brooklyn.api.entity.ImplementedBy;
 import org.apache.brooklyn.api.sensor.AttributeSensor;
-import org.apache.brooklyn.core.entity.trait.Startable;
 import org.apache.brooklyn.core.sensor.Sensors;
 
-import java.util.Map;
-
 @ImplementedBy(ManagedResourceImpl.class)
-public interface ManagedResource extends Startable, TerraformResource {
+public interface ManagedResource extends TerraformResource {
 
     AttributeSensor<String> RESOURCE_STATUS = Sensors.newStringSensor("tf.resource.status",
             "The status of this resource");
 
-    boolean refreshSensors(Map<String,Object> resource);
-
-    void updateResourceState();
 }
--- src/main/java/io/cloudsoft/terraform/entity/ManagedResourceImpl.java
+++ src/main/java/io/cloudsoft/terraform/entity/ManagedResourceImpl.java
@@ -1,93 +1,34 @@
 package io.cloudsoft.terraform.entity;
 
-import com.google.common.base.Predicates;
-import com.google.common.collect.ImmutableList;
-import org.apache.brooklyn.api.location.Location;
-import org.apache.brooklyn.core.annotation.Effector;
-import org.apache.brooklyn.core.config.ConfigKeys;
 import org.apache.brooklyn.core.entity.Attributes;
 import org.apache.brooklyn.core.entity.lifecycle.Lifecycle;
 import org.apache.brooklyn.core.sensor.Sensors;
-import org.apache.brooklyn.entity.software.base.SoftwareProcess;
 import org.apache.brooklyn.entity.stock.BasicEntityImpl;
 
-import java.util.Collection;
 import java.util.Map;
 
-public class ManagedResourceImpl extends BasicEntityImpl implements ManagedResource {
-
-    public static final ImmutableList<String> ACCEPTED_STATE = ImmutableList.of("ok", "running", "up", "online");
+public class ManagedResourceImpl extends BasicEntityImpl implements  ManagedResource {
 
     @Override
     public void init() {
         super.init();
         connectSensors();
-        config().set(ConfigKeys.builder(String.class)
-                .name("destination")
-                .description("Where to POST the messages.")
-                .constraint(Predicates.notNull())
-                .defaultValue("http://ness_ip:8181")
-                .reconfigurable(true)
-                .build() , "");
-    }
-
-    @Effector(description = "[TBD]Start the resource based on the type.")
-    @Override
-    public void start(Collection<? extends Location> locations) {
-        // TODO Do we even want to allow control from AMP?
-        // start means different things for different resource types.
-        // TODO consider executing an operation based on the resource type
-        connectSensors();
     }
 
     protected void connectSensors() {
-        Map<String, Object> resourceDetails = this.getConfig(ManagedResource.STATE_CONTENTS);
+        Map<String, Object> resourceDetails = this.getConfig(StartableManagedResource.STATE_CONTENTS);
         resourceDetails.forEach((k,v) -> sensors().set(Sensors.newStringSensor("tf." + k), v.toString()));
         if(!resourceDetails.containsKey("resource.status")) {
             sensors().set(RESOURCE_STATUS, "ok"); // the provider doesn't provide any property to let us know the state of the resource
         }
-        sensors().set(SERVICE_UP, Boolean.TRUE);
-        this.setDisplayName(getConfig(ManagedResource.ADDRESS));
-        updateResourceState();
+        this.setDisplayName(getConfig(StartableManagedResource.ADDRESS));
+        sensors().set(Attributes.SERVICE_STATE_ACTUAL, Lifecycle.CREATED);
     }
 
     @Override
     public boolean refreshSensors(Map<String, Object> resource) {
         resource.forEach((k, v) -> sensors().set(Sensors.newStringSensor("tf." + k), v.toString()));
-        updateResourceState();
+        sensors().set(RESOURCE_STATUS, "ok");
         return true;
-    }
-
-    @Effector(description = "[TBD]Stop the resource based on the type.")
-    @Override
-    public void stop() {
-        // TODO Do we even want to allow control from AMP?
-        // stop means different things for different resource types.
-        // TODO consider executing an operation based on the resource type
-    }
-
-    @Effector(description = "[TBD]Restart the resource based on the type.")
-    @Override
-    public void restart() {
-        // figure out how to do this - get the location from Resource !? Use terrraform ?
-        // check comments on stop as well
-        // TODO Do we even want to allow control from AMP?
-    }
-
-    public void updateResourceState(){
-        final String resourceStatus = sensors().get(RESOURCE_STATUS);
-        if (ACCEPTED_STATE.contains(resourceStatus)) {
-            sensors().set(SoftwareProcess.SERVICE_PROCESS_IS_RUNNING, Boolean.TRUE);
-            sensors().set(Attributes.SERVICE_STATE_ACTUAL, Lifecycle.RUNNING);
-        } else if (resourceStatus.equalsIgnoreCase(Lifecycle.STOPPING.name())) {
-            sensors().set(SoftwareProcess.SERVICE_PROCESS_IS_RUNNING, Boolean.FALSE);
-            sensors().set(Attributes.SERVICE_STATE_ACTUAL, Lifecycle.STOPPING);
-        } else if (resourceStatus.equalsIgnoreCase(Lifecycle.STOPPED.name())) {
-            sensors().set(SoftwareProcess.SERVICE_PROCESS_IS_RUNNING, Boolean.FALSE);
-            sensors().set(Attributes.SERVICE_STATE_ACTUAL, Lifecycle.STOPPED);
-        } else  {
-            sensors().set(SoftwareProcess.SERVICE_PROCESS_IS_RUNNING, Boolean.TRUE);
-            sensors().set(Attributes.SERVICE_STATE_ACTUAL, Lifecycle.ON_FIRE);
-        }
     }
 }
